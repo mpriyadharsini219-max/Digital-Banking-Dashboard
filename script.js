65a4c33ca464ddcafdbdaacf8d6fc0383a126e47
PRIYA DHARSINI M <mpriyadharsini219@gmail.com>
2026-09-02 20:19:09 +0530

    Add financial feature functions in script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function sendMoney() {
+    alert("Send Money feature selected.");
+}
+
+function payBill() {
+    alert("Pay Bills feature selected.");
+}
+
+function addMoney() {
+    alert("Add Money feature selected.");
+}
+
+function downloadStatement() {
+    alert("Your statement is ready to download.");
+}
+
+function showAllTransactions() {
+    alert("Showing all transactions.");
+}
